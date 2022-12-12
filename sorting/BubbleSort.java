1aa7997c61e588e96853a999a7234271509a1fa8
MANH <manhmanh310599@gmail.com>
2022-12-12 09:40:29 +0700

    1

---
+++ sorting/BubbleSort.java
@@ -0,0 +1,42 @@
+package sorting;
+
+/**
+ * BubbleSort
+ */
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        int [] a = {9,7,8,5,6,4,1,3,2};
+        SapXep(a);
+        
+
+    }
+
+    private static void SapXep(int[] a) {
+
+            int n = a.length;
+
+            for (int i = 0;i<n;i++)
+            {
+                for (int j = 0;j<n-i-1;j++)
+                {
+                    if (a[j]>a[j+1])
+                    {
+                        int t = a[j];
+                        a[j] = a[j+1];
+                        a[j+1] = t;
+                    }
+                }
+                inRaMang(i,a);
+            }
+    }
+
+    private static void inRaMang(int i, int[] a) {
+        System.out.printf("%d: ",i);
+        for (int j = 0;j<a.length;j++)
+        {
+            System.out.printf("%d ",a[j]);
+        }
+        System.out.println("\n");
+    }
+}
